79505155bf2bdbfe36852bb7d14d2b11af9da1f7
mahmoudgit24 <mahmoudaymn6000@gmail.com>
2024-07-11 02:25:26 +0300

    add main.js file

---
+++ main.js
@@ -0,0 +1,25 @@
+//let=parseInt(prompt("enter N1"))
+//let=parseInt(prompt("enter N2"))
+
+//console.log(n1-n2)
+//console.log(n1*n2)
+//console.log(n1/n2)
+//console.log(n1%n2)
+
+let n1 =+prompt ("")  , n2 =+prompt("") ;
+
+let resulte =`
+${n1} + ${n2} =${n1+n2}
+${n1} - ${n2} =${n1-n2}
+${n1} * ${n2} =${n1*n2}
+${n1} / ${n2} =${n1/n2}
+${n1} % ${n2} =${n1%n2}
+
+`
+alert(resulte)
+
+console.log(typeof n1);
+ 
+let name="ahmed"
+
+console.log(n1);
